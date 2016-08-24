cee4e247c4f06a4ea7fe587233c2273b1e857cbe
Sherryl Santoso <Patrisia0593@gmail.com>
2016-08-24 11:54:29 +0800

    Update saveVideo.py

--- python/saveVideo.py
+++ python/saveVideo.py
@@ -1,16 +1,16 @@
-#To save Video in MP4 format using Open CV
+#To Capture Video using webcam and save it in MP4 format using Open CV
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 width=cap.get(3)
 height=cap.get(4)
+
 out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 30.0, (640,480))
-frame_counter=0
+
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-        frame_counter+=1
         frame = cv2.flip(frame,180)
         # write the flipped frame
         out.write(frame)
