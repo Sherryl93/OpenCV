da42ce7dcfbcc4a4faea2c57e95ff144ea20dcb7
Sherryl Santoso <Patrisia0593@gmail.com>
2016-08-26 08:48:18 +0800

    Create cropCenter.py

---
+++ python/images/cropCenter.py
@@ -0,0 +1,17 @@
+## Function to crop the center of Images
+import numpy as np
+
+def centeredCrop(img):
+	height =  np.size(img,0)
+	width =  np.size(img,1)
+  
+  ##set new Width and Height size:
+	new_width= (int)(0.15*width) 
+	new_height=(int)(0.15*height)
+
+	left = np.ceil((width - new_width)/2.)
+	top = np.ceil((height - new_height)/2.)
+	right = width - np.floor((width - new_width)/2.)
+	bottom = height - np.floor((height - new_height)/2.)
+	cImg = img[top:bottom, left:right]
+	return cImg
