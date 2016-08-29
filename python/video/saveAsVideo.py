87ad35c4ecf954581028d96148e7defbfe0df8b4
Sherryl Santoso <Patrisia0593@gmail.com>
2016-08-29 16:23:56 +0800

    Update saveAsVideo.py

--- python/video/saveAsVideo.py
+++ python/video/saveAsVideo.py
@@ -13,8 +13,7 @@ out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 30.0, (width
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-        frame = cv2.flip(frame,180)
-        # write the flipped frame
+        # write the frame
         out.write(frame)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):  #to exit the loop by pressing key "q"
