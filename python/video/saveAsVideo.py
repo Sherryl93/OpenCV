144fbf9a853971fa6153367e4fe11caa8ef0196c
Sherryl Santoso <Patrisia0593@gmail.com>
2016-11-03 09:36:36 +0800

    Update saveAsVideo.py

--- python/video/saveAsVideo.py
+++ python/video/saveAsVideo.py
@@ -8,7 +8,10 @@ height=cap.get(4)
 width=int(width)
 height=int(height)
 
+
 out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 30.0, (width,height))
+##If no module VideoWriter_fourcc found use this instead:
+#out = cv2.VideoWriter('output.mp4',cv2.cv.CV_FOURCC(*'MP4V'), 30.0, (width,height))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
